2d6097966f9c1c5ee0b37629f15be2dd5ccb74f9
kouki kawabata <kouki@dm.cs.kumamoto-u.ac.jp>
2017-04-27 00:58:18 +0900

    tmp

--- load_data.py
+++ load_data.py
@@ -3,16 +3,22 @@ import os
 import sys
 import argparse
 import calendar
+import datetime
 sys.path.append('../settings/')
 import pandas as pd
 import fitbit as fb
 import user
 
-def load_hr():
-    print('hoge')
 
 def dict2df(raw):
     return pd.DataFrame.from_dict(raw)
+
+def load_hr(day, month, year, level='1sec'):
+    # haert rate
+    date = '-'.join([year, str(month).zfill(2), str(day).zfill(2)])
+    dat = client.intraday_time_series('activities/heart', date, detail_level=level)
+    dat = dat["activities-heart-intraday"]["dataset"]
+    return dict2df(dat)
 
 
 if __name__ == '__main__':
@@ -20,24 +26,28 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download fitbit data.')
     parser.add_argument('--heart', type=int, help='download heart rate')
     parser.add_argument('--steps', type=int, help='download # of steps')
-    parser.add_argument('-d', '--day', type=int, help='')
-    parser.add_argument('-m', '--month', type=int, help='')
-    parser.add_argument('year', type=int, help='')
+    parser.add_argument('-y', '--year', type=str, help='')
+    parser.add_argument('-m', '--month', type=str, help='')
+    parser.add_argument('-d', '--day', type=str, help='')
     parser.add_argument('outdir', help='output directory')
     args = parser.parse_args()
 
+    if not args.year:
+        
     cal = calendar.Calendar()
     if args.month:
         # print(args.month)
         if args.day:
             print(args.day)
+            load_hr(args.day, args.month, args.year)
         else:
             for d in cal.itermonthdays(args.year, args.month):
-                if d:
-                    print(d)
+                if not d: continue
+                load_hr(d, args.month, args.year)
     else:
         for m in range(1, 13):
             print(m)
+            load_hr(args.day, args.month, args.year)
 
     # # setting
     # client = fb.Fitbit(user.fitbit_id,
@@ -45,13 +55,6 @@ if __name__ == '__main__':
     #                    access_token=user.fitbit_access_token,
     #                    refresh_token=user.fitbit_refresh_token)
     
-    # # haert rate
-    # dat = client.intraday_time_series('activities/heart', '2017-04-01', detail_level='1sec')
-    # hr = dat["activities-heart-intraday"]["dataset"]
-    # df = dict2df(hr)
-    # print(df.shape)
-    # df.head()
-
     # df.index = pd.to_datetime(['2017-04-01' + " " + t for t in df.time])
     # df.head()
     # print(df)
